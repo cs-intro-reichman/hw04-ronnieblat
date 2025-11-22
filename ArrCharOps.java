fc447df7a001c01190452c759f87a3622b9d212c
ronnie blat <ronnie.blat@post.runi.ac.il>
2025-11-22 09:29:45 +0200

    fix2

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -153,7 +153,7 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        if (str1 == null || str2 == null)
+        if (str1 == null || str2 == null||str1.length()==0||str2.length()==0)
             return -2;
         int i=0;
         for (i=0; i<Math.min(str1.length(),str2.length()); i++){
